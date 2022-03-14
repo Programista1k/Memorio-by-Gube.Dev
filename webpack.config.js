a1dc2a365cc1ebddd971c45fa3123e971b16d297
Programista1k <mfvpolska@gmail.com>
2022-03-14 20:44:21 +0100

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+const webpack = require("webpack");
+const path = require("path");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+
+const config = {
+  mode: "development",
+  entry: "./scripts/app.ts",
+  output: {
+    filename: "app.js",
+    path: path.resolve(__dirname, "dist"),
+    publicPath: "/dist",
+  },
+  devtool: "inline-source-map",
+  devServer: {
+    static: {
+      directory: path.join(__dirname),
+    },
+    client: {
+      logging: "none",
+    },
+    hot: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+      {
+        test: /\.ts(x)?$/,
+        loader: "ts-loader",
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
+  plugins: [new CleanWebpackPlugin()],
+};
+
+module.exports = config;
